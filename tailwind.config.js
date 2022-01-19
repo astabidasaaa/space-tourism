5b282c7adfe2094bf334fbcf9f0a4c1713bd2893
astabidasaaa <astabidasabcd@gmail.com>
2022-01-19 15:24:19 +0700

    update basic config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: "var(--darkBlue)",
+        secondary: "var(--paleBlue)",
+        tertiary: "var(--white)",
+      },
+    },
+  },
+  plugins: [],
+};
