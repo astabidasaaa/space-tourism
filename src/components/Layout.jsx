8f905b524ce6a5a14aec2214ff9f063e18825290
astabidasaaa <astabidasabcd@gmail.com>
2022-02-12 21:43:59 +0700

    remove clg author

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,6 @@ import GlobalStyles from "../styles/GlobalStyles";
 import Header from "./Header";
 import tw, { styled } from "twin.macro";
 import { motion, AnimatePresence } from "framer-motion";
-import { useEffect } from "react";
 
 const MainBody = styled(motion.main)(() => [
   tw`box-border flex flex-col flex-nowrap justify-end items-center flex-grow w-full h-full relative`,
@@ -11,21 +10,6 @@ const MainBody = styled(motion.main)(() => [
 
 const Layout = ({ children }) => {
   const keyLocation = children.props.location.pathname.split("/")[1];
-  useEffect(() => {
-    const cssRules = [
-      "color: #D0D6F9",
-      "background: #0B0D17",
-      "font-family: sans-serif",
-      "font-size: 16px",
-      "padding: 16px",
-    ].join(";");
-
-    console.group("author");
-    console.log("%ccoded by sngkr", cssRules);
-    console.log("https://sngkr.netlify.app/");
-    console.log("%cFrontend Mentor Challenge", "font-weight: 900;");
-    console.groupEnd("author");
-  }, []);
 
   return (
     <>
